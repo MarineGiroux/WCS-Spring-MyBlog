27165d99a2b284dcef0c2ab334b2fd1590d4d25e
MarineGiroux <85103261+MarineGiroux@users.noreply.github.com>
2025-01-13 11:05:56 +0100

    added RestController and sayHello()

--- src/main/java/org/wildcodeschool/myblog/MyBlogApplication.java
+++ src/main/java/org/wildcodeschool/myblog/MyBlogApplication.java
@@ -2,7 +2,10 @@ package org.wildcodeschool.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MyBlogApplication {
 
@@ -10,4 +13,8 @@ public class MyBlogApplication {
 		SpringApplication.run(MyBlogApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World!";
+	}
 }
